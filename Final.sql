569a7915c3cef3ad0fe49a017ce6f2ff8b507d26
miguelangellujan <miguel.lujan@edu.uah.es>
2024-12-01 12:07:26 +0100

    tablas final y temporal

--- Final.sql
+++ Final.sql
@@ -1,3 +1,91 @@
-create database PECL2;
-create schema if not exists final
-
+create table if not exists collision_crashes_def(
+    accident_id SERIAL PRIMARY KEY,
+    CRASH_DATE date not null,
+    CRASH_TIME  time not null ,
+    borough Varchar(30),
+    zip_code CHAR(5),
+    latitude NUMERIC(9,6),
+    longitude NUMERIC(9,6),
+    on_street_name	VARCHAR(100),
+    cross_street_name	VARCHAR(100),
+    off_street_name	VARCHAR(100),
+    persons_injured	smallint,
+    persons_killed	smallint,
+    pedestrians_injured	smallint,
+    pedestrians_killed	smallint,
+    cyclists_injured	smallint,
+    cyclists_killed	smallint,
+    motorists_injured	smallint,
+    motorists_killed	smallint,
+    CONTRIBUTING_FACTOR_VEHICLE_1 VARCHAR(255),
+    CONTRIBUTING_FACTOR_VEHICLE_2 VARCHAR(255),
+    CONTRIBUTING_FACTOR_VEHICLE_3 VARCHAR(255),
+    CONTRIBUTING_FACTOR_VEHICLE_4 VARCHAR(255),
+    CONTRIBUTING_FACTOR_VEHICLE_5 VARCHAR(255),
+    Constraint collision_crashes_def PRIMARY KEY(accident_id)
+);
+create table if not exists vehicles_def(
+    vehicle_id	SERIAL PRIMARY KEY,
+    state_registration CHAR(2),
+    vehicle_type	VARCHAR(20),
+    vehicle_make	VARCHAR(20),
+    vehicle_model	VARCHAR(30),
+    vehicle_year	smallint,
+    Constraint vehicles_def PRIMARY KEY(vehicle_id)
+);
+Create table if not exists personas2_def(
+    person_id SERIAL PRIMARY KEY,
+    person_sex	CHAR(1),
+    person_lastname	VARCHAR(20),
+    person_firstname VARCHAR(20),
+    person_phone	VARCHAR(15),
+    person_address	VARCHAR(100),
+    person_city	VARCHAR(30)	,
+    person_state CHAR(2),
+    person_zip	CHAR(5)	,
+    person_ssn	CHAR(11),
+    person_dob	DATE,
+    Constraint personas2_def PRIMARY KEY(person_id)
+);
+CREATE TABLE IF NOT EXISTS collision_person_def (
+    collision_person_id SERIAL PRIMARY KEY,
+    accident_id INT,
+    person_id INT,
+    vehicle_id INT,
+    person_type VARCHAR(20),
+    person_sex CHAR(1),
+    person_injury VARCHAR(50),
+    person_age SMALLINT,
+    ejection BOOLEAN,
+    emotional_status VARCHAR(50),
+    bodily_injury VARCHAR(50),
+    position_in_vehicle VARCHAR(50),
+    safety_equipment VARCHAR(50),
+    ped_location VARCHAR(50),
+    ped_action VARCHAR(50),
+    COMPLAINT VARCHAR(255),
+    ped_role VARCHAR(50),
+    CONTRIBUTING_FACTOR_1 VARCHAR(255),
+    CONTRIBUTING_FACTOR_2 VARCHAR(255),
+    CONSTRAINT fk_accident FOREIGN KEY (accident_id) REFERENCES collision_crashes_def(accident_id),
+    CONSTRAINT fk_person FOREIGN KEY (person_id) REFERENCES personas2_def(person_id),
+    CONSTRAINT fk_vehicle FOREIGN KEY (vehicle_id) REFERENCES vehicles_def(vehicle_id)
+);
+CREATE TABLE IF NOT EXISTS collision_vehicles_def (
+    collision_vehicle_id SERIAL PRIMARY KEY,
+    accident_id INT,
+    vehicle_id INT,
+    travel_direction VARCHAR(50),
+    vehicle_occupants SMALLINT,
+    driver_sex CHAR(1),
+    driver_license_status VARCHAR(20),
+    driver_license_jurisdiction VARCHAR(50),
+    pre_crash VARCHAR(50),
+    point_of_impact VARCHAR(50),
+    public_property_damage VARCHAR(100),
+    public_property_damage_type VARCHAR(100),
+    CONTRIBUTING_FACTOR_1 VARCHAR(255),
+    CONTRIBUTING_FACTOR_2 VARCHAR(255),
+    CONSTRAINT fk_accident FOREIGN KEY (accident_id) REFERENCES collision_crashes_def(accident_id),
+    CONSTRAINT fk_vehicle FOREIGN KEY (vehicle_id) REFERENCES vehicles_def(vehicle_id)
+);
--- Temporal.sql
+++ Temporal.sql
@@ -1,2 +1,118 @@
 create database PECL2;
-create schema if not exists temporal
+create schema if not exists final;
+create schema if not exists temp;
+CREATE TABLE IF NOT EXISTS collision_crashes (
+    CRASH_DATE TEXT,
+    CRASH_TIME TEXT,
+    borough TEXT,
+    zip_code TEXT,
+    latitude TEXT,
+    longitude TEXT,
+    location TEXT,
+    on_street_name TEXT,
+    cross_street_name TEXT,
+    off_street_name TEXT,
+    persons_injured TEXT,
+    persons_killed TEXT,
+    pedestrians_injured TEXT,
+    pedestrians_killed TEXT,
+    cyclists_injured TEXT,
+    cyclists_killed TEXT,
+    motorists_injured TEXT,
+    motorists_killed TEXT,
+    CONTRIBUTING_FACTOR_VEHICLE_1 TEXT,
+    CONTRIBUTING_FACTOR_VEHICLE_2 TEXT,
+    CONTRIBUTING_FACTOR_VEHICLE_3 TEXT,
+    CONTRIBUTING_FACTOR_VEHICLE_4 TEXT,
+    CONTRIBUTING_FACTOR_VEHICLE_5 TEXT,
+    accidente_id TEXT,
+    vehicle_type_code_1 TEXT,
+    vehicle_type_code_2 TEXT,
+    vehicle_type_code_3 TEXT,
+    vehicle_type_code_4 TEXT,
+    vehicle_type_code_5 TEXT
+);
+CREATE TABLE IF NOT EXISTS vehicles (
+    vehicle_id TEXT,
+    vehicle_type TEXT,
+    vehicle_make TEXT,
+    vehicle_model TEXT,
+    vehicle_year TEXT
+);
+CREATE TABLE IF NOT EXISTS personas2(
+    person_id TEXT,
+    person_sex TEXT,
+    person_lastname TEXT,
+    person_firstname TEXT,
+    person_phone TEXT,
+    person_address TEXT,
+    person_city TEXT,
+    person_state TEXT,
+    person_zip TEXT,
+    person_ssn TEXT,
+    person_dob TEXT
+);
+CREATE TABLE IF NOT EXISTS collision_person(
+    collision_vehicle_id TEXT,
+    accident_id TEXT,
+    crash_date TEXT,
+    crash_time TEXT,
+    person_id TEXT,
+    person_type TEXT,
+    person_injury TEXT,
+    vehicle_id TEXT,
+    person_age TEXT,
+    ejection TEXT,
+    emotional_status TEXT,
+    bodily_injury TEXT,
+    position_in_vehicle TEXT,
+    safety_equipment TEXT,
+    ped_location TEXT,
+    ped_action TEXT,
+    COMPLAINT TEXT,
+    ped_role TEXT,
+    CONTRIBUTING_FACTOR TEXT,
+    CONTRIBUTING_FACTOR_2 TEXT,
+    person_sex TEXT
+);
+CREATE TABLE IF NOT EXISTS collision_vehicles (
+    collision_vehicle_id TEXT,
+    accident_id TEXT,
+    crash_date TEXT,
+    crash_time TEXT,
+    vehicle_id TEXT,
+    state_registration TEXT,
+    vehicle_type TEXT,
+    vehicle_make TEXT,
+    vehicle_model TEXT,
+    vehicle_year TEXT,
+    travel_direction TEXT,
+    vehicle_occupants TEXT,
+    driver_sex TEXT,
+    driver_license_status TEXT,
+    driver_license_jurisdiction TEXT,
+    pre_crash TEXT,
+    point_of_impact TEXT,
+    vehicle_damage TEXT,
+    vehicle_damage_2 TEXT,
+    vehicle_damage_3 TEXT,
+    vehicle_damage_4 TEXT,
+    public_property_damage TEXT,
+    public_property_damage_type TEXT,
+    CONTRIBUTING_FACTOR TEXT,
+    CONTRIBUTING_FACTOR_2 TEXT
+);
+
+COPY temp.collision_crashes FROM 'C:\Collisions_Crashes_20241020.csv'
+WITH CSV HEADER NULL '' DELIMITER ',';
+
+COPY pecl2.temp.vehicles FROM 'C:\Vehicles.csv'
+    WITH CSV HEADER NULL '' DELIMITER ';';
+COPY temp.personas2 FROM 'C:\personas2.csv'
+    WITH CSV HEADER NULL '' DELIMITER ';';
+
+COPY pecl2.temp.collision_person FROM 'C:\Collisions_Person_20241020.csv'
+    WITH CSV HEADER NULL '' DELIMITER ',';
+
+COPY pecl2.temp.collision_vehicles FROM 'C:\Collisions_Vehicles_20241020.csv'
+    WITH CSV HEADER NULL '' DELIMITER ',';
